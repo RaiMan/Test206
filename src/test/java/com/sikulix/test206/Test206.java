77588dfebdda03d70a7f27a5eed2b5551df8ee57
RaiMan <sikulix@outlook.com>
2022-02-08 10:04:14 +0100

    ongoing

--- src/test/java/com/sikulix/test206/Test206.java
+++ src/test/java/com/sikulix/test206/Test206.java
@@ -3,6 +3,7 @@ package com.sikulix.test206;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.MethodOrderer.DisplayName;
 import org.sikuli.basics.Debug;
+import org.sikuli.basics.Settings;
 import org.sikuli.script.*;
 import org.sikuli.script.Image;
 import org.sikuli.script.support.Commons;
@@ -11,6 +12,8 @@ import org.sikuli.script.support.gui.SXDialog;
 
 import java.awt.*;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -70,33 +73,6 @@ public class Test206 {
 
   static String appRun = "idea";
 
-  static void show(SXDialog dialog) {
-    show(dialog,0,0);
-  }
-
-  static void show(SXDialog dialog, long when) {
-    show(dialog, when,0);
-  }
-
-  static void show(SXDialog dialog, long when, long time) {
-    screen = new Region(0, 0, sxDialogImagesSize.width, sxDialogImagesSize.height);
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        if (when > 0) {
-          Commons.pause(when);
-        }
-        dialog.setAlwaysOnTop(true);
-        dialog.run();
-        if (time > 0) {
-          Commons.pause(time);
-          dialog.dispose();
-        }
-      }
-    }).start();
-    Commons.pause(1);
-  }
-
   boolean trace() {
     StackTraceElement stackTrace = Thread.currentThread().getStackTrace()[2];
     String className = stackTrace.getFileName().replace(".java", "");
@@ -117,6 +93,27 @@ public class Test206 {
     Commons.info(msg, args);
   }
 
+  void highlight(Element element) {
+    if (!verbose) {
+      return;
+    }
+    if (element instanceof Region) {
+      ((Region) element).highlight(1);
+    }
+  }
+
+  void highlight(Element element, List<Match> matches) {
+    if (!verbose) {
+      return;
+    }
+    if (element instanceof Region) {
+      boolean actionLogs = Settings.ActionLogs;
+      Settings.ActionLogs = false;
+      ((Region) element).highlight(matches, 2);
+      Settings.ActionLogs = actionLogs;
+    }
+  }
+
   @Test
   void test001_find_Image() {
     if (!trace()) return;
@@ -129,6 +126,7 @@ public class Test206 {
       assertTrue(false);
     }
   }
+
   @Test
   void test002_findResized_Image() {
     if (!trace()) return;
@@ -147,6 +145,7 @@ public class Test206 {
       assertTrue(false);
     }
   }
+
   @Test
   void test005_getAll_Image() {
     if (!trace()) return;
@@ -158,10 +157,29 @@ public class Test206 {
     }
     assertEquals(expected, matches.size());
   }
+
+  @Test
+  void test006_getAnyAll_Image() { //TODO minScore 0,94 (Region: 0,98) ???
+    if (!trace()) return;
+    //verbose = true;
+    double minScore = 1;
+    Image image = Image.create("SikulixTest001");
+    List<Object> targets = Arrays.asList(new String[]{"img", "img100"});
+    List<List<Match>> matchesList = image.getAnyAll(targets);
+    List<Match> matches = matchesList.get(0);
+    matches.addAll(matchesList.get(1));
+    for (Match match : matches) {
+      info("%d: %s %s", match.getIndex(), match, match.getImage().getName());
+      minScore = Math.min(minScore, match.getScore());
+    }
+    boolean indexOK = matches.get(0).getIndex() == 0 && matches.get(matches.size() - 1).getIndex() == 1;
+    assertTrue(matches.size() == 7 && indexOK);
+  }
+
   @Test
   void test011_find_Region() {
     if (!trace()) return;
-    show(sxDialogImages);
+    SXDialog.onScreen(sxDialogImages);
     try {
       Match match = screen.find("img");
       info("%s %s", match, match.getImage());
@@ -170,11 +188,12 @@ public class Test206 {
       assertTrue(false);
     }
   }
+
   @Test
   void test012_findAgain_Region() {
     if (!trace()) return;
     Image.create("img").setLastSeen(null);
-    show(sxDialogImages);
+    SXDialog.onScreen(sxDialogImages);
     try {
       Match match = screen.find("img");
       info("%s %s", match, match.getImage());
@@ -185,10 +204,11 @@ public class Test206 {
       assertTrue(false);
     }
   }
+
   @Test
   void test014_wait_Region() {
     if (!trace()) return;
-    show(sxDialogImages, 2);
+    SXDialog.onScreen(sxDialogImages, 2);
     try {
       Match match = screen.wait("img");
       info("%s %s", match, match.getImage());
@@ -197,27 +217,29 @@ public class Test206 {
       assertTrue(false);
     }
   }
+
   @Test
   void test018_waitVanish_Region() {
     if (!trace()) return;
-    show(sxDialogImages, 0, 2);
+    SXDialog.onScreen(sxDialogImages, 0, 2);
     boolean success = screen.waitVanish("img");
     assertTrue(success);
   }
+
   @Test
   void test019_waitVanishNotThere_Region() {
     if (!trace()) return;
     boolean success = screen.waitVanish("img");
     assertTrue(!success);
   }
+
   @Test
-  void test031_findAll_Region() {
+  void test021_findAll_Region() {
     if (!trace()) return;
-    show(sxDialogImages);
+    SXDialog.onScreen(sxDialogImages);
     Match match;
     int expected = 6;
     int actual = 0;
-    //Commons.startTrace();
     try {
       Iterator<Match> finderIterator = screen.findAll("img");
       while (finderIterator.hasNext()) {
@@ -230,10 +252,11 @@ public class Test206 {
       assertTrue(false);
     }
   }
+
   @Test
   void test022_getAll_Region() {
     if (!trace()) return;
-    show(sxDialogImages);
+    SXDialog.onScreen(sxDialogImages);
     int expected = 6;
     List<Match> matches = screen.getAll("img");
     for (Match match : matches) {
@@ -241,15 +264,97 @@ public class Test206 {
     }
     assertEquals(expected, matches.size());
   }
+
   @Test
   void test023_getAllWait_Region() {
     if (!trace()) return;
-    show(sxDialogImages, 2);
+    SXDialog.onScreen(sxDialogImages, 2);
     int expected = 6;
-    List<Match> matches = screen.getAll(3,"img");
+    List<Match> matches = screen.getAll(3, "img");
     for (Match match : matches) {
       info("%s %s", match, match.getImage());
     }
     assertEquals(expected, matches.size());
   }
+
+  @Test
+  void test031_getAny_Region() {
+    if (!trace()) return;
+    //verbose = true;
+    double minScore = 1;
+    SXDialog.onScreen(sxDialogImages);
+    List<Object> targets = Arrays.asList("sikulix-red", "img", "sikulix-red", "img100");
+    List<Match> matches = screen.getAny(targets);
+    highlight(screen, matches);
+    for (Match match : matches) {
+      info("%d: %s %s", match.getIndex(), match, match.getImage().getName());
+      minScore = Math.min(minScore, match.getScore());
+    }
+    boolean indexOK = matches.get(0).getIndex() == 1 && matches.get(1).getIndex() == 3;
+    assertTrue(matches.size() == 2 && indexOK && minScore > 0.99);
+  }
+
+  @Test
+  void test032_getAnyAll_Region() {
+    if (!trace()) return;
+    //verbose = true;
+    double minScore = 1;
+    SXDialog.onScreen(sxDialogImages);
+    List<Object> targets = Arrays.asList("img", "img100");
+    List<List<Match>> matchesList = screen.getAnyAll(targets);
+    List<Match> matches = matchesList.get(0);
+    matches.addAll(matchesList.get(1));
+    highlight(screen, matches);
+    for (Match match : matches) {
+      info("%d: %s %s", match.getIndex(), match, match.getImage().getName());
+      minScore = Math.min(minScore, match.getScore());
+    }
+    boolean indexOK = matches.get(0).getIndex() == 0 && matches.get(matches.size() - 1).getIndex() == 1;
+    assertTrue(matches.size() == 7 && indexOK && minScore > 0.98);
+  }
+
+  @Test
+  void test033_getBest_Region() {
+    if (!trace()) return;
+    //verbose = true;
+    SXDialog.onScreen(sxDialogImages);
+    List<Object> targets = Arrays.asList("img200", "img", "img100");
+    Match match = screen.getBest(targets);
+    highlight(match);
+    info("%d: %s %s", match.getIndex(), match, match.getImage().getName());
+    assertEquals(1, match.getIndex());
+  }
+
+  @Test
+  void test034_waitBest_Region() {
+    if (!trace()) return;
+    //verbose = true;
+    SXDialog.onScreen(sxDialogImages, 2);
+    List<Object> targets = Arrays.asList("img200", "img", "img100");
+    Match match = screen.waitBest(3, targets);
+    if (match != null) {
+      highlight(match);
+      info("%d: %s %s", match.getIndex(), match, match.getImage().getName());
+      assertEquals(1, match.getIndex());
+    } else {
+      assertTrue(false);
+    }
+  }
+  @Test
+  void test035_waitAny_Region() {
+    if (!trace()) return;
+    //verbose = true;
+    double minScore = 1;
+    SXDialog.onScreen(sxDialogImages, 2);
+    List<Object> targets = Arrays.asList("sikulix-red", "img", "sikulix-red", "img100");
+    List<Match> matches = screen.waitAny(3, targets);
+    highlight(screen, matches);
+    for (Match match : matches) {
+      info("%d: %s %s", match.getIndex(), match, match.getImage().getName());
+      minScore = Math.min(minScore, match.getScore());
+    }
+    boolean indexOK = matches.get(0).getIndex() == 1 && matches.get(1).getIndex() == 3;
+    assertTrue(matches.size() == 2 && indexOK && minScore > 0.99);
+  }
+
 }
